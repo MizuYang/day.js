0857a1b150be4c313da06c7870c6161a874a5220
Mizu <psdk55052004@gmail.com>
2023-11-14 16:33:22 +0800

    新增 列出該語系的月份、週

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -34,6 +34,40 @@
     </header>
 
     <main>
+      <!-- 列出該語系的月份、週 -->
+      <section class="bg-gainsboro mb-3 p-10">
+        <h2 class="text-20 mb-3">列出該語系的月份、週</h2>
+        <div class="bg-whitesmoke mb-3 p-3">
+          <h3 class="text-18 fw-bold-7 mb-1">dayjs.weekdays</h3>
+          <p class="p-2">
+            {{ weekdays }}
+          </p>
+        </div>
+        <div class="bg-whitesmoke mb-3 p-3">
+          <h3 class="text-18 fw-bold-7 mb-1">dayjs.weekdaysShort</h3>
+          <p class="p-2">
+            {{ weekdaysShort }}
+          </p>
+        </div>
+        <div class="bg-whitesmoke mb-3 p-3">
+          <h3 class="text-18 fw-bold-7 mb-1">dayjs.weekdaysMin</h3>
+          <p class="p-2">
+            {{ weekdaysMin }}
+          </p>
+        </div>
+        <div class="bg-whitesmoke mb-3 p-3">
+          <h3 class="text-18 fw-bold-7 mb-1">dayjs.monthsShort</h3>
+          <p class="p-2">
+            {{ monthsShort }}
+          </p>
+        </div>
+        <div class="bg-whitesmoke mb-3 p-3">
+          <h3 class="text-18 fw-bold-7 mb-1">dayjs.months</h3>
+          <p class="p-2">
+            {{ months }}
+          </p>
+        </div>
+      </section>
     </main>
   </div>
 </template>
@@ -44,9 +78,18 @@ import dayjs from 'dayjs'
 import 'dayjs/locale/zh-cn'
 import 'dayjs/locale/ja'
 
+import localeData from 'dayjs/plugin/localeData'
+
 // data
 const now = dayjs()
 const lang = ref('zh-cn')
+// 列出該語系的月份、週
+const weekdays = ref('')
+const weekdaysShort = ref('')
+const weekdaysMin = ref('')
+const monthsShort = ref('')
+const months = ref('')
+
 console.log('now', now)
 
 // watchEffect
@@ -55,7 +98,43 @@ watchEffect(() => {
   // dayjs().locale(lang.value).format() // 局部修改语言配置
   dayjs.locale(lang.value)
   console.log('當前的語系為：', dayjs.locale())
+
+  列出該語系的月份和週()
 })
+
+dayjs.extend(localeData)
+
+dayjs().localeData()
+
+列出該語系的月份和週()
+
+function 列出該語系的月份和週 () {
+  if (!dayjs.weekdays) return
+  if (!dayjs.weekdaysShort) return
+  if (!dayjs.weekdaysMin) return
+  if (!dayjs.monthsShort) return
+  if (!dayjs.months) return
+
+  weekdays.value = dayjs.weekdays()
+  console.log(dayjs.weekdays())
+  // ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六']
+
+  weekdaysShort.value = dayjs.weekdaysShort()
+  console.log(dayjs.weekdaysShort())
+  // ['周日', '周一', '周二', '周三', '周四', '周五', '周六']
+
+  weekdaysMin.value = dayjs.weekdaysMin()
+  console.log(dayjs.weekdaysMin())
+  // ['日', '一', '二', '三', '四', '五', '六']
+
+  monthsShort.value = dayjs.monthsShort()
+  console.log(dayjs.monthsShort())
+  // ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月']
+
+  months.value = dayjs.months()
+  console.log(dayjs.months())
+// ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月']
+}
 </script>
 
 <style lang='scss' scope></style>
