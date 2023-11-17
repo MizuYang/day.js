118a76f86e8d0441ef98c8b0ca605ad278d3e6f3
Mizu <psdk55052004@gmail.com>
2023-11-17 16:18:06 +0800

    新增 Demo7: 年月日自動轉換

--- src/components/Demo7輸入時間自動轉換單位.vue
+++ src/components/Demo7輸入時間自動轉換單位.vue
@@ -2,12 +2,8 @@
   <section class="bg-gainsboro mb-3 p-10">
     <h2 class="text-20 mb-3">7. 時間自動轉換</h2>
 
-    <div>
-      <!-- 年 -->
-      <!-- 月 -->
-      <!-- 日 -->
-      <!-- 最終轉換的日期、時間 -->
-    </div>
+    <!-- 年月日自動轉換 -->
+    <YearChange />
 
     <!-- 時分秒自動轉換 -->
     <TimeChange />
@@ -16,7 +12,9 @@
 </template>
 
 <script setup>
+import YearChange from '@/components/demo7-items/年月日自動轉換.vue'
 import TimeChange from '@/components/demo7-items/時分秒自動轉換.vue'
+
 </script>
 
 <style lang='scss' scope></style>
---
+++ src/components/demo7-items/年月日自動轉換.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="d-flex align-items-center mb-3">
+    <!-- 年 -->
+    <label for="demo7Hour" class="d-flex align-items-center">
+      <input type="number"
+              class="form-control text-center mx-2"
+              id="demo7Hour"
+              placeholder="請輸入小時"
+              v-model="data.years"
+              style="width:100px;">
+      <span class="mx-3">年</span>
+    </label>
+
+    <!-- 月 -->
+    <label for="demo7Hour" class="d-flex align-items-center">
+      <input type="number"
+              class="form-control text-center mx-2"
+              id="demo7Hour"
+              placeholder="請輸入小時"
+              v-model="data.months"
+              style="width:100px;">
+      <span class="mx-3">月</span>
+    </label>
+
+    <!-- 日 -->
+    <label for="demo7Hour" class="d-flex align-items-center">
+      <input type="number"
+              class="form-control text-center mx-2"
+              id="demo7Hour"
+              placeholder="請輸入小時"
+              v-model="data.date"
+              style="width:100px;">
+      日
+    </label>
+
+    <!-- 最終轉換的日期、時間 -->
+    <span class="mx-5"
+          style="font-size:20px;">=</span>
+    <!-- 轉換結果 -->
+    <p class="bg-whitesmoke fw-bold-7 py-2 px-3">{{ result }}</p>
+
+  </div>
+</template>
+
+<script setup>
+import { ref, reactive, watchEffect } from 'vue'
+import dayjs from 'dayjs'
+
+// 匯入 dayjs 的 plugin
+import objectSupport from 'dayjs/plugin/objectSupport'
+
+// dayjs 的 plugin 註冊
+dayjs.extend(objectSupport)
+
+// data
+const data = reactive({
+  years: 2010,
+  months: 3,
+  date: 5
+})
+const result = ref('')
+
+// watchEffect
+watchEffect(() => {
+  result.value = dayjs(data).format('YYYY-MM-DD')
+})
+</script>
+
+<style lang='scss' scope></style>
